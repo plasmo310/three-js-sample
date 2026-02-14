368610b24bb9552a8042503fab60b614ac963a6d
plasmo310 <plasmo310@gmail.com>
2026-02-14 13:40:55 +0900

    complete multi prim material.

--- 03_multi_prim_material/js/main_scene.js
+++ 03_multi_prim_material/js/main_scene.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { Cube } from "./cube.js";
 
 /**
  * Main Scene
@@ -17,6 +16,7 @@ export class MainScene {
       intensity: 0.5,
     });
     this.scene.add(aoLight);
+
     const dirLight = new THREE.DirectionalLight({
       color: 0xffffff,
       intensity: 0.5,
@@ -25,52 +25,105 @@ export class MainScene {
     });
     this.scene.add(dirLight);
 
-    // TODO: material variation.
-    this.materials = [
+    // material variation.
+    const matVariation = [
       new THREE.MeshBasicMaterial({
         color: "white",
         wireframe: true,
       }),
       new THREE.MeshBasicMaterial({
         color: "white",
+        side: THREE.DoubleSide,
       }),
       new THREE.MeshLambertMaterial({
         color: "white",
+        side: THREE.DoubleSide,
       }),
       new THREE.MeshPhongMaterial({
         color: "white",
-        metalness: 10.0,
+        side: THREE.DoubleSide,
+        shininess: 80,
         specular: new THREE.Color("white"),
       }),
       new THREE.MeshNormalMaterial({
-        color: "white",
+        side: THREE.DoubleSide,
       }),
     ];
 
-    // TODO: geometry variation.
-    this.geometries = [];
+    // heart geometry.
+    const heartShape = new THREE.Shape();
+    const x = -2.5;
+    const y = -5;
+    heartShape.moveTo(x + 2.5, y + 2.5);
+    heartShape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+    heartShape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+    heartShape.bezierCurveTo(
+      x - 3,
+      y + 5.5,
+      x - 1.5,
+      y + 7.7,
+      x + 2.5,
+      y + 9.5,
+    );
+    heartShape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+    heartShape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+    heartShape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+    const heartExtrudeSettings = {
+      steps: 2,
+      depth: 2,
+      bevelEnabled: true,
+      bevelThickness: 1,
+      bevelSize: 1,
+      bevelSegments: 2,
+    };
+    const heartGeometry = new THREE.ExtrudeGeometry(
+      heartShape,
+      heartExtrudeSettings,
+    );
+    heartGeometry.scale(0.075, 0.075, 0.075);
 
-    // created actors.
-    this.actors = [];
+    // tube geometry.
+
+    // geometry variation.
+    const geoVariation = [
+      new THREE.BoxGeometry(0.65, 0.65, 0.65),
+      new THREE.SphereGeometry(0.5, 12, 6),
+      new THREE.CircleGeometry(0.5, 24, Math.PI * 0.25, Math.PI * 1.5),
+      new THREE.CylinderGeometry(0.2, 0.35, 0.75),
+      new THREE.TorusKnotGeometry(0.35, 0.15, 8, 10, 5, 5),
+      heartGeometry,
+    ];
+
+    // created meshes.
+    this.meshes = [];
 
     // offset values.
-    const OFFSET_POS_UNIT_Y = 1.25;
-    let posOffsetY = (OFFSET_POS_UNIT_Y * (this.materials.length - 1)) / 2;
+    const OFFSET_POS_UNIT_X = 1.75;
+    const OFFSET_POS_UNIT_Y = 1.35;
 
-    for (const material of this.materials) {
-      const actor = new Cube(material);
-      this.scene.add(actor.mesh);
-      this.actors.push(actor);
+    // create meshes.
+    let posOffsetX = -(OFFSET_POS_UNIT_X * (geoVariation.length - 1)) / 2;
+    for (const geometry of geoVariation) {
+      let posOffsetY = (OFFSET_POS_UNIT_Y * (matVariation.length - 1)) / 2;
+      for (const material of matVariation) {
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.position.x = posOffsetX;
+        mesh.position.y = posOffsetY;
 
-      actor.mesh.position.y = posOffsetY;
-      posOffsetY -= OFFSET_POS_UNIT_Y;
+        this.scene.add(mesh);
+        this.meshes.push(mesh);
+
+        posOffsetY -= OFFSET_POS_UNIT_Y;
+      }
+      posOffsetX += OFFSET_POS_UNIT_X;
     }
   }
 
   onUpdate(deltaTime) {
-    // update objects.
-    for (const actor of this.actors) {
-      actor.onUpdate(deltaTime);
+    for (const mesh of this.meshes) {
+      // rotate.
+      mesh.rotation.x += 1 * deltaTime;
+      mesh.rotation.y += 1 * deltaTime;
     }
   }
 
